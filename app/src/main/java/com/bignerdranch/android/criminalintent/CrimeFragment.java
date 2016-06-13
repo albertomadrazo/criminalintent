61faf12b904161867d652bb3e45cf06b891385b0
unknown <it04@IT04.FESA.com>
2016-06-13 10:42:16 -0500

    First commit. Up to creating Crime class.

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.criminalintent;
+
+
+import android.support.v4.app.Fragment;
+
+// asegúrate de escoger la clase correcta a heredar
+public class CrimeFragment extends Fragment{
+    
+}
