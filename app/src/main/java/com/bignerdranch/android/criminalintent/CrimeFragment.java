60587876b07fd25c0c3c85a686a9ef657537bfaa
unknown <it04@IT04.FESA.com>
2016-06-13 11:39:52 -0500

    Adds CrimeFragment.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -3,13 +3,17 @@ package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 // asegúrate de escoger la clase correcta a heredar
 public class CrimeFragment extends Fragment{
     private Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -20,6 +24,25 @@ public class CrimeFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This space intntionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count){
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This one too
+            }
+        });
+
         return v;
     }
 }
