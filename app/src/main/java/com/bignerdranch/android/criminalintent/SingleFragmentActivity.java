8cf7edc457ded06d3b98ba07efb773af76ced978
Alberto Madrazo <betomadrazo@gmail.com>
2016-06-16 21:47:51 -0500

    Adelanta el capitulo de fragmentos. Crea CrimeLab.java y SingleFragmentActivity.java

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -1,24 +1,25 @@
 package com.bignerdranch.android.criminalintent;
 
+
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
 
-public class CrimeActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
-        // You call getSupportFragmentManager() because you are using the support library and the
-        // FragmentActivity class.
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if(fragment == null){
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
