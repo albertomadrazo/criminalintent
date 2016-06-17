6762a340eb15e0f1998e43455af2920f1dccdaa7
unknown <it04@IT04.FESA.com>
2016-06-17 14:50:00 -0500

    Recycler view.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,9 +1,6 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
 
 
 public class CrimeActivity extends SingleFragmentActivity {
