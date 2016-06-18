bfa6cf360fc6df57f326ea4522b67244d09fea30
Alberto Madrazo <betomadrazo@gmail.com>
2016-06-18 00:51:39 -0500

    Add a log.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 
@@ -8,8 +9,14 @@ public class CrimeActivity extends SingleFragmentActivity {
     private static final String TAG = "### CrimeActivity ###";
 
     @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate()");
+    }
+
+    @Override
     protected Fragment createFragment(){
-        Log.i(TAG, "nalgonas");
+        Log.i(TAG, "createFragment()");
         return new CrimeFragment();
     }
 }
