1934a1d56419f0db562c6121e24ad40d28f4a764
unknown <it04@IT04.FESA.com>
2016-06-30 16:03:40 -0500

    End of Lesson 13. Add two buttons to the layout.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -2,6 +2,8 @@ package com.bignerdranch.android.criminalintent;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.CursorWrapper;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import com.bignerdranch.android.criminalintent.database.CrimeCursorWrapper;
 import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 
@@ -44,12 +47,37 @@ public class CrimeLab {
 
     public List<Crime> getCrimes(){
 
-        return new ArrayList<>();
+        List<Crime> crimes = new ArrayList<>();
+        CrimeCursorWrapper cursor = (CrimeCursorWrapper) queryCrimes(null, null);
+
+        try{
+            cursor.moveToFirst();
+            while(!cursor.isAfterLast()){
+                crimes.add(cursor.getCrime());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+
+        return crimes;
     }
 
     public Crime getCrime(UUID id){
+        CrimeCursorWrapper cursor = (CrimeCursorWrapper) queryCrimes(
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] {id.toString()}
+        );
 
-        return null;
+        try{
+            if(cursor.getCount() == 0){
+                return null;
+            }
+            cursor.moveToFirst();
+            return cursor.getCrime();
+        } finally {
+            cursor.close();
+        }
     }
 
     public void updateCrime(Crime crime){
@@ -69,5 +97,19 @@ public class CrimeLab {
         values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
 
         return values;
+    }
+
+    private CursorWrapper queryCrimes(String whereClause, String[] whereArgs){
+        Cursor cursor = mDatabase.query(
+                CrimeTable.NAME, // table
+                null,            // columns    - null selects all columns
+                whereClause,     // where
+                whereArgs,       // whereArgs
+                null,            // groupBy
+                null,            // having
+                null             // orderBy
+        );
+
+        return new CrimeCursorWrapper(cursor);
     }
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -119,6 +119,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else{
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
 
@@ -179,7 +180,12 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public int getItemCount(){
+
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes){
+            mCrimes = crimes;
         }
     }
 }
